a55d0ae6c4653a9513a3a07537cdade09ba381eb
sabnk <sabnk@optictelecom.ru>
2023-12-04 22:55:17 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("my-java-library")
+}
+
+group = "org.example"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "my-project"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+    }
+    includeBuild("../my-build-logic")
+}
+
+dependencyResolutionManagement{
+    repositories {
+        mavenCentral()
+    }
+    //includeBuild("../my-other-project")
+}
+
+rootDir.listFiles()?.filter{it.isDirectory && !it.isHidden}?.forEach{
+    include(it.name)
+}
+
+//include("data-model")
+//include("business-logic")
+//include("app")
